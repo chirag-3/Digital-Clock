23c85667c09756337f74430a86aae8003e4ca2f9
chirag <chirag42001@gmail.com>
2023-04-16 00:03:30 +0530

    24 hour format chamged to 12 hour format project completed

--- myscripts.js
+++ myscripts.js
@@ -15,6 +15,23 @@ function changeTime()
     var m = today.getMinutes();
     var s = today.getSeconds();
 
+
+    if(h>=12)
+    {
+        var meridiem = document.getElementById('am-pm');
+        meridiem.innerHTML = 'PM';
+    }
+    else
+    {
+        var meridiem = document.getElementById('am-pm');
+        meridiem.innerHTML = 'AM';
+    }
+    
+    if(h==0)
+      h = 12;
+    if(h>12)
+      h = h - 12;
+
     hour.innerHTML = h>=10?h.toString() : ('0' + h.toString());
     minutes.innerHTML = m>=10?m.toString() : ('0' + m.toString());
     seconds.innerHTML = s>=10?s.toString() : ('0' + s.toString());
